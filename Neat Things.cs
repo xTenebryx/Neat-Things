f368cede26b20e8a614dd28fec8e1878a7ee9a2e
xTenebryx <maksim.korolk@gmail.com>
2026-08-01 02:41:07 +0300

    removed assembly reference, I hope

--- Neat Things.cs
+++ Neat Things.cs
@@ -6,7 +6,7 @@ namespace NeatThings
 {
     public static class NeatThings 
     {
-        public const string MOD_VERSION = "1.0.0"; // Set this to the version of your mod.
+        public const string MOD_VERSION = "1.0.1"; // Set this to the version of your mod.
         public const string CONFIG_VERSION = "1.0"; // Increment this whenever you change your mod's config file.
     }
 }
